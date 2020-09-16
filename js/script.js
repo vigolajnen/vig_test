7fc150651869790cc6a936288d3aa8193632e164
Anya Vigolajnen <vigolajnen@gmail.com>
2020-09-16 21:30:04 +0300

    add files

---
+++ js/script.js
@@ -0,0 +1,13 @@
+'use strict';
+
+(function () {
+  if ('NodeList' in window && !NodeList.prototype.forEach) {
+
+    NodeList.prototype.forEach = function (callback, thisArg) {
+      thisArg = thisArg || window;
+      for (var i = 0; i < this.length; i++) {
+        callback.call(thisArg, this[i], i, this);
+      }
+    };
+  }
+})();
